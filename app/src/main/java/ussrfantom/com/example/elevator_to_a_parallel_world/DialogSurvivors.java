8f0ee58bf06ae06a94fbd04fffd64b824eca5f79
Ussrfantom <dimkakrohin@mail.ru>
2020-10-02 00:23:44 +0300

    add firebase

--- app/src/main/java/ussrfantom/com/example/elevator_to_a_parallel_world/DialogSurvivors.java
+++ app/src/main/java/ussrfantom/com/example/elevator_to_a_parallel_world/DialogSurvivors.java
@@ -1,25 +1,58 @@
 package ussrfantom.com.example.elevator_to_a_parallel_world;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DialogSurvivors extends AppCompatActivity {
     private ListView listViewSurvivors;
-   ArrayList <String> arrayList;
+    private ArrayList <String> arrayList;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog_survivors);
-     arrayList = new ArrayList<>();
-     arrayList.add("Гоша");
+
+        db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", "иван");
+        user.put("lastname", "иванjdbx");
+        user.put("age", 15);
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        System.out.println("УСПЕШНО! <------------------------------------");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println("провал <------------------------------------"+ e);
+                    }
+                });
+
+
+        arrayList = new ArrayList<>();
+        arrayList.add("Гоша");
         arrayList.add("Гоша");
         arrayList.add("Гоша");
         arrayList.add("Гоша");
