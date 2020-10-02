f57a79e227724614031733e65dc23b8fa2cc6ebf
Ussrfantom <dimkakrohin@mail.ru>
2020-10-03 02:13:58 +0300

    receiving data and displaying it in the view is done in the past the game section

--- app/src/main/java/ussrfantom/com/example/elevator_to_a_parallel_world/DialogSurvivors.java
+++ app/src/main/java/ussrfantom/com/example/elevator_to_a_parallel_world/DialogSurvivors.java
@@ -3,19 +3,20 @@ package ussrfantom.com.example.elevator_to_a_parallel_world;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.events.Event;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,47 +29,48 @@ public class DialogSurvivors extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog_survivors);
-
+        arrayList = new ArrayList<>(); //массив для вывода текста
         db = FirebaseFirestore.getInstance();
-        Map<String, Object> user = new HashMap<>();
-        user.put("name", "иван");
-        user.put("lastname", "иванjdbx");
-        user.put("age", 15);
+        listViewSurvivors = findViewById(R.id.listViewSurvivors);//ссылка на view с измененным текстом
 
+        //получение данных из базы
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(DialogSurvivors.this, "Успешно", Toast.LENGTH_SHORT).show();
+                    QuerySnapshot querySnapshot = task.getResult();
+                    if (querySnapshot == null) return;
+                    for (QueryDocumentSnapshot documentSnapshot:querySnapshot){
+                        Map<String, Object> user = documentSnapshot.getData();
+                        arrayList.add(user.get("1").toString());
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(DialogSurvivors.this, R.layout.layouttextcolorwhite, arrayList);
+                    listViewSurvivors.setAdapter(adapter);
+                }else{
+                    Toast.makeText(DialogSurvivors.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
+
+//добавление данных
+/*
         db.collection("users")
                 .add(user)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        System.out.println("УСПЕШНО! <------------------------------------");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        System.out.println("провал <------------------------------------"+ e);
-                    }
-                });
-
-
-        arrayList = new ArrayList<>();
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        arrayList.add("Гоша");
-        listViewSurvivors = findViewById(R.id.listViewSurvivors);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(DialogSurvivors.this, R.layout.layouttextcolorwhite, arrayList);
-        listViewSurvivors.setAdapter(adapter);
-    }
-}
+@Override
+public void onSuccess(DocumentReference documentReference) {
+        System.out.println("УСПЕШНО! <------------------------------------");
+        }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+@Override
+public void onFailure(@NonNull Exception e) {
+        System.out.println("провал <------------------------------------"+ e);
+        }
+        });
+*/
